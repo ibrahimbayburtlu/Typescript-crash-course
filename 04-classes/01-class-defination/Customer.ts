3be5eb914a925e4953d46a290036f2469b5ce9a1
İbrahim <ibrahimbayburtlu5@gmail.com>
2023-04-18 14:36:17 +0300

    Customer

--- 04-classes/01-class-defination/Customer.ts
+++ 04-classes/01-class-defination/Customer.ts
@@ -1,18 +1,37 @@
 class Customer{
 
-    firstName: string;
-    lastName: string;
+    private _firstName: string;
+    private _lastName: string;
 
     constructor(theFirst: string, theLast: string){
-        this.firstName = theFirst;
-        this.lastName = theLast;
+        this._firstName = theFirst;
+        this._lastName = theLast;
     }
+
+    public getFirstName():string{
+        return this._firstName;
+    }
+
+    public setFirstName(theFirst:string):void{
+        this._firstName = theFirst;
+    }
+
+    public getLastName():string{
+        return this._lastName;
+    }
+
+    public setLastName(theLast:string):void{
+        this._lastName = theLast;
+    }
+
 }
 
 // let's create an instance
 
 let myCustomer = new Customer("Martin","Dixon");
 
-console.log(myCustomer.firstName);
+console.log(myCustomer.getFirstName());
 
-console.log(myCustomer.lastName);
+myCustomer.setLastName("Selen");
+
+console.log(myCustomer.getLastName());
