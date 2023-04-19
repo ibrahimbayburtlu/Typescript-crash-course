b26eb146201bb66bcdce5b3b3343040bb5cf2881
İbrahim <ibrahimbayburtlu5@gmail.com>
2023-04-19 10:58:36 +0300

    abstract

---
+++ 06-abstract-classes/Rectangle.ts
@@ -0,0 +1,35 @@
+import { Shape } from "./Shape";
+import { Circle } from "./Circle";
+
+
+export class Rectangle extends Shape{
+
+    calculatorArea(): number {
+        return this._width * this._length;
+    }
+
+
+    constructor(theX:number,theY:number,
+        private _width : number , private _length: number){
+            super(theX,theY);
+    }
+
+    public get width() {
+        return this._width;
+    }
+    public set width(value) {
+        this._width = value;
+    }
+
+    public get length(): number {
+        return this._length;
+    }
+    public set length(value: number) {
+        this._length = value;
+    }
+
+    
+    getInfo(): string {
+        return super.getInfo() + ` width=${this._width}, length=${this._length}`;
+    }
+} 
